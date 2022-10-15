22bb75811960f05c76432c1f0a568b23434caea8
Ubuntu <maffei.filippo102@gmail.com>
2022-10-15 15:44:02 +0000

    Updated lists

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 date
-cd /home/pi/repos/cgdata 
+cd /mnt/data/cgdata 
 #sleep $((RANDOM % 1800))
 curl -m 60 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false' -A "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36" -o first.json 2>&1
 #sleep $((RANDOM % 900))
